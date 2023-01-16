622ba8d3ad6b0cf564daf5613872794031791aae
Leonardo Rasile <rasile.leonardo@gmail.com>
2023-01-16 16:04:24 +0000

    Format changes 2023-01-16T16:04:24

--- git.ps1
+++ git.ps1
@@ -2,5 +2,5 @@ param (
     # name of the output image
     [string]$m = 'Updates'
 )
-$date = Get-Date -format "yyyy-MM-dd hh:mm"
+$date = Get-Date -format "s"
 git status; git add .; git commit -m "$($m) $($date)"; git push; git status
