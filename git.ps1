5bf51f16d084886fb5de51cb3fa233f1e64276cc
Leonardo Rasile <rasile.leonardo@gmail.com>
2023-01-16 16:01:05 +0000

    This is my commit message 2023-01-16 04:01

--- git.ps1
+++ git.ps1
@@ -2,5 +2,5 @@ param (
     # name of the output image
     [string]$m = 'Updates'
 )
-$date = Get-Date -format "yyyyMMdd"
+$date = Get-Date -format "yyyy-MM-dd hh:mm"
 git status; git add .; git commit -m "$($m) $($date)"; git push; git status
