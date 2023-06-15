75b1de94f6fc061de95d47942c71e3f59b41082b
Leonardo Rasile <rasile.leonardo@gmail.com>
2023-06-15 22:09:04 +0100

    layout Changes - 2023-06-15T22:09:04

--- pages/Prototypes.tsx
+++ pages/Prototypes.tsx
@@ -4,44 +4,63 @@ import { useRouter } from 'next/router'
 export default function Apps() {
   const router = useRouter()
 
-  const styleTile: CSSProperties = { textAlign: 'center', cursor: 'pointer' }
+  const styleTile: CSSProperties = {
+    textAlign: 'center',
+    cursor: 'pointer',
+    background: 'rgba(150, 150, 150, 0.1)',
+    border: '0px solid rgba(150, 150, 150, 0.8)',
+    borderRadius: 10,
+    height: 200,
+    minWidth: 200,
+  }
 
   return (
-    <div className="flex-container">
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/JungleClears')}>
-        <img style={{ margin: 'auto', padding: 10 }} height={150} width={150} src="../images/JungleIcon.png" />
-        <div className="tileText">Jungle Clears</div>
-      </div>
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/PokemonEffectiveness')}>
-        <div style={{ height: 150, paddingTop: 20 }}>
-          <div className="Pokeball" style={{ margin: 'auto' }}>
-            <div className="PokeballLine"></div>
-            <div className="PokeballDot"></div>
-          </div>
+    <div className="row justify-content-center">
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/JungleClears')}>
+          <img style={{ margin: 'auto', padding: 10 }} height={150} width={150} src="../images/JungleIcon.png" />
+          <div className="tileText">Jungle Clears</div>
         </div>
-        <div className="tileText"> Pokemon Effectiveness</div>
       </div>
-
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/LoopHeroSolver')}>
-        <img style={{ margin: 'auto', padding: 10 }} height={150} width={150} src="../images/LoopHeroLogo.png" />
-        <div className="tileText">Loop Hero</div>
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/PokemonEffectiveness')}>
+          <div style={{ height: 150, paddingTop: 20 }}>
+            <div className="Pokeball" style={{ margin: 'auto' }}>
+              <div className="PokeballLine"></div>
+              <div className="PokeballDot"></div>
+            </div>
+          </div>
+          <div className="tileText">Pokemon Effectiveness</div>
+        </div>
       </div>
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/CardsForBeats')}>
-        <img
-          style={{ margin: 'auto', paddingTop: 40 }}
-          height={150}
-          width={150}
-          src="../images/cardsForBeats/LSBL.png"
-        />
-        <div className="tileText">Cards For Beats</div>
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/LoopHeroSolver')}>
+          <img style={{ margin: 'auto', padding: 10 }} height={150} width={150} src="../images/LoopHeroLogo.png" />
+          <div className="tileText">Loop Hero</div>
+        </div>
       </div>
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/Menu')}>
-        <span style={{ margin: 'auto', paddingTop: 10, fontSize: 100 }}>🍝</span>
-        <div className="tileText">Menu</div>
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/CardsForBeats')}>
+          <img
+            style={{ margin: 'auto', paddingTop: 40 }}
+            height={150}
+            width={150}
+            src="../images/cardsForBeats/LSBL.png"
+          />
+          <div className="tileText">Cards For Beats</div>
+        </div>
       </div>
-      <div className="flex-item" style={styleTile} onClick={() => router.push('Prototypes/Timezones')}>
-        <span style={{ margin: 'auto', paddingTop: 10, fontSize: 100 }}>⏰</span>
-        <div className="tileText">Timezones</div>
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/Menu')}>
+          <span style={{ margin: 'auto', paddingTop: 10, fontSize: 100 }}>🍝</span>
+          <div className="tileText">Menu</div>
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 my-2">
+        <div style={styleTile} onClick={() => router.push('Prototypes/Timezones')}>
+          <span style={{ margin: 'auto', paddingTop: 10, fontSize: 100 }}>⏰</span>
+          <div className="tileText">Timezones</div>
+        </div>
       </div>
     </div>
   )
