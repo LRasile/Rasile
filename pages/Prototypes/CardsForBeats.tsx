a1f1876d467812c632b807c984626d92c65eb063
Leonardo Rasile <rasile.leonardo@gmail.com>
2023-06-16 00:00:13 +0100

    beat Changes - 2023-06-16T00:00:13

--- pages/Prototypes/CardsForBeats.tsx
+++ pages/Prototypes/CardsForBeats.tsx
@@ -17,8 +17,13 @@ import {
   ModalOverlay,
   Input,
   Text,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Box,
 } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import BeatGroup from '../../components/CardsForBeats/BeatGroup'
 import {
   BeatGroupEntity,
@@ -40,13 +45,9 @@ export default function CardsForBeats() {
   const [numberOfGroups, setNumberOfGroups] = useState<number>(4)
   const { isOpen, onOpen, onClose } = useDisclosure()
   const toast = useToast()
-  const [userSettingObj, setUserSettingObj] =
-    useState<UserSettings>(defaultSettings)
+  const [userSettingObj, setUserSettingObj] = useState<UserSettings>(defaultSettings)
 
-  const [userSettings, setUserSettings] = useCookie(
-    'settings',
-    JSON.stringify(defaultSettings)
-  )
+  const [userSettings, setUserSettings] = useCookie('settings', JSON.stringify(defaultSettings))
 
   useEffect(() => {
     if (userSettings) {
@@ -64,11 +65,7 @@ export default function CardsForBeats() {
       setNumberOfGroups(numberOfGroupsQuery)
       setNumberOfBeats(typeQuery)
 
-      const newGroups = BuildGroupsFromPhrase(
-        pharseQuerySplit[2],
-        numberOfGroupsQuery,
-        typeQuery
-      )
+      const newGroups = BuildGroupsFromPhrase(pharseQuerySplit[2], numberOfGroupsQuery, typeQuery)
       setGroups(newGroups)
     }
   }, [router.query.phrase])
@@ -121,9 +118,7 @@ export default function CardsForBeats() {
 
     if (groups && groups.length > 0) {
       phraseQueryString += GetPhaseString()
-      navigator.clipboard.writeText(
-        `${origin}${url}?phrase=${phraseQueryString}`
-      )
+      navigator.clipboard.writeText(`${origin}${url}?phrase=${phraseQueryString}`)
       return true
     }
     return false
@@ -145,24 +140,83 @@ export default function CardsForBeats() {
   function ReloadPhrase() {
     console.log('ReloadPhrase')
     console.log(GetPhaseString())
-    const newGroups = BuildGroupsFromPhrase(
-      GetPhaseString(),
-      numberOfGroups,
-      numberOfBeats
-    )
+    const newGroups = BuildGroupsFromPhrase(GetPhaseString(), numberOfGroups, numberOfBeats)
     setGroups(newGroups)
   }
 
   // Metronome ------------------------------------------------------------------------
-
   const [tempo, setTempo] = useState<number>(120) // initial tempo is 120 bpm
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
-  const [intervalId, setIntervalId] = useState(null)
-  const [audioContext, setAudioContext] = useState<AudioContext>(null)
+  const [intervalId, setIntervalId] = useState<number | null>(null)
+  const audioContextRef = useRef<AudioContext | null>(null) // Use a ref for AudioContext
+  const oscillatorRef = useRef<OscillatorNode | null>(null) // Use a ref for OscillatorNode
+
   const [beatIndex, setBeatIndex] = useState<number>(0)
-  const [totalNumberOfNotes, setTotalNumberOfNotes] = useState<number>(
-    numberOfBeats * numberOfGroups
-  )
+  const [totalNumberOfNotes, setTotalNumberOfNotes] = useState<number>(numberOfBeats * numberOfGroups)
+
+  useEffect(() => {
+    let context = null
+
+    // Check for AudioContext availability and fallback if needed
+    if (typeof window.AudioContext !== 'undefined') {
+      context = new AudioContext()
+    } else if (typeof window.webkitAudioContext !== 'undefined') {
+      context = new window.webkitAudioContext()
+    } else {
+      // Web Audio API is not supported
+      console.error('Web Audio API is not supported in this environment.')
+      return
+    }
+
+    audioContextRef.current = context
+
+    const osc = audioContextRef.current.createOscillator() // Use the ref to access AudioContext
+    osc.type = 'square' // Set oscillator type to square wave
+    oscillatorRef.current = osc
+  }, [])
+
+  const playTick = (isAccent) => {
+    const osc = audioContextRef.current.createOscillator()
+    const amp = audioContextRef.current.createGain()
+
+    osc.connect(amp) // Connect the oscillator to the gain node
+    amp.connect(audioContextRef.current.destination) // Connect the gain node to the audio context destination
+
+    const baseFrequency = isAccent ? 600 : 500
+    const clickDuration = 0.05 // Duration of the click sound
+
+    // Set up the click noise by rapidly changing the oscillator frequency
+    const clickFrequency = 5000 // Frequency for the click noise
+    const clickInterval = 0.001 // Interval between frequency changes
+    const numClicks = Math.ceil(clickDuration / clickInterval)
+
+    // Schedule frequency changes to create the click noise
+    for (let i = 0; i < numClicks; i++) {
+      const startTime = audioContextRef.current.currentTime + i * clickInterval
+      const endTime = startTime + clickInterval
+
+      osc.frequency.setValueAtTime(baseFrequency, startTime)
+      osc.frequency.setValueAtTime(clickFrequency, endTime)
+    }
+
+    // Set the final frequency at the end of the click noise
+    const finalTime = audioContextRef.current.currentTime + clickDuration
+    osc.frequency.setValueAtTime(baseFrequency, finalTime)
+
+    // Configure gain node for the click noise
+    const clickAttackTime = 0.001 // Duration of the attack (ramp up)
+    const clickReleaseTime = 0.01 // Duration of the release (ramp down)
+
+    amp.gain.setValueAtTime(0, audioContextRef.current.currentTime) // Set initial gain to 0
+
+    // Apply envelope shaping for the click noise
+    amp.gain.linearRampToValueAtTime(1, audioContextRef.current.currentTime + clickAttackTime) // Ramp up the gain to 1
+    amp.gain.linearRampToValueAtTime(0, finalTime + clickReleaseTime) // Ramp down the gain to 0
+
+    // Start and stop the oscillator for the duration of the click noise
+    osc.start(audioContextRef.current.currentTime)
+    osc.stop(finalTime + clickReleaseTime)
+  }
 
   useEffect(() => {
     setTotalNumberOfNotes(numberOfBeats * numberOfGroups)
@@ -170,66 +224,58 @@ export default function CardsForBeats() {
   }, [numberOfBeats, numberOfGroups])
 
   useEffect(() => {
-    // initialize the audio context
-    const context = new AudioContext()
-    setAudioContext(context)
-  }, [])
-
-  const playTick = (pitch) => {
-    const osc = audioContext.createOscillator()
-    const gain = audioContext.createGain() // create gain node
-    osc.frequency.value = pitch // set frequency to 1000 Hz for a sharper sound
-    osc.type = 'square' // set oscillator type to square wave for a sharper sound
-    osc.connect(gain) // connect the oscillator to the gain node
-    gain.connect(audioContext.destination) // connect the gain node to the audio context
-    gain.gain.value = 0.1 // set the gain value to 0.1 to reduce volume
-    osc.start(audioContext.currentTime)
-    osc.stop(audioContext.currentTime + 0.05) // stop after 0.05 seconds for a shorter sound
-  }
-
-  let accentedPitch = 1100
-  let normalPitch = 900
-
-  useEffect(() => {
-    let intervalId
+    let timeoutId = null // Add this variable to keep track of the timeout
 
     if (isPlaying) {
-      // Play the first tick immediately
-      playTick(normalPitch)
-      setBeatIndex(-2)
+      // Play the count-in ticks
+      playTick(false)
+      playTick(false)
+      playTick(false)
 
-      // Set the interval for the remaining ticks
-      const interval = (60 / tempo) * 1000 // interval in ms
-      intervalId = setInterval(() => {
+      setBeatIndex(-3) // Adjust the beat index for the count-in
+
+      // Set the interval for the regular ticks
+      const interval = 60 / tempo / numberOfBeats // Calculate the interval based on the tempo and beats per measure
+      let nextTickTime = audioContextRef.current.currentTime + interval
+
+      function scheduleTick() {
         setBeatIndex((beatCount) => {
-          let pitch =
-            beatCount % numberOfBeats === 0 ? accentedPitch : normalPitch
+          let isAccent = beatCount % numberOfBeats === 0
           if (beatCount === totalNumberOfNotes) {
             resetMetronome()
             return 1
           } else {
-            playTick(pitch)
+            playTick(isAccent)
             return beatCount + 1
           }
         })
-      }, interval)
+
+        nextTickTime += interval
+
+        // Schedule the next tick
+        if (isPlaying) {
+          timeoutId = setTimeout(scheduleTick, (nextTickTime - audioContextRef.current.currentTime) * 1000)
+        }
+      }
+
+      scheduleTick()
     } else {
       resetMetronome()
     }
 
-    // Clear interval when the component unmounts or is re-rendered
-    return () => clearInterval(intervalId)
+    // Clear timeout when the component unmounts or is re-rendered
+    return () => clearTimeout(timeoutId)
   }, [isPlaying, tempo, totalNumberOfNotes])
 
   function resetMetronome() {
-    clearInterval(intervalId)
+    clearTimeout(intervalId)
     setIntervalId(null)
     setBeatIndex(0)
     setIsPlaying(false)
   }
 
-  const handleTempoChange = (event) => {
-    const newTempo = parseInt(event.target.value)
+  const handleTempoChange = (value) => {
+    const newTempo = parseInt(value)
     if (!isNaN(newTempo) && newTempo > 0 && newTempo <= 300) {
       setTempo(newTempo)
       resetMetronome()
@@ -277,12 +323,22 @@ export default function CardsForBeats() {
           <div className="col-md-3 my-1">
             <FormControl>
               <FormLabel>Tempo (BPM)</FormLabel>
-              <Input
-                type="number"
-                defaultValue={tempo}
-                onChange={handleTempoChange}
-                maxW="400px"
-              />
+              <Box display="flex" alignItems="center">
+                <Slider
+                  defaultValue={tempo}
+                  onChange={(value) => handleTempoChange(value)}
+                  min={1}
+                  max={300}
+                  step={1}
+                  colorScheme="blue"
+                >
+                  <SliderTrack>
+                    <SliderFilledTrack />
+                  </SliderTrack>
+                  <SliderThumb />
+                </Slider>
+                <Box ml={4}>{tempo} BPM</Box>
+              </Box>
             </FormControl>
           </div>
           <div className="col-md-3 my-1">
@@ -314,12 +370,7 @@ export default function CardsForBeats() {
               >
                 <Icon as={BsShareFill} />
               </IconButton>
-              <IconButton
-                mx="1"
-                aria-label="Settings"
-                onClick={onOpen}
-                title="Settings"
-              >
+              <IconButton mx="1" aria-label="Settings" onClick={onOpen} title="Settings">
                 <SettingsIcon />
               </IconButton>
 
@@ -328,19 +379,11 @@ export default function CardsForBeats() {
               </IconButton>
               {isPlaying && (
                 <>
-                  <IconButton
-                    onClick={handleStopClick}
-                    aria-label="Stop"
-                    mx="1"
-                  >
+                  <IconButton onClick={handleStopClick} aria-label="Stop" mx="1">
                     <FaStop />
                   </IconButton>
-                  {beatIndex < 1 && (
-                    <Text fontSize="xl">{(beatIndex - 1) * -1 * 1}</Text>
-                  )}
-                  {beatIndex >= 1 && (
-                    <Text fontSize="xl">Follow the beat!</Text>
-                  )}
+                  {beatIndex < 1 && <Text fontSize="xl">{(beatIndex - 1) * -1 * 1}</Text>}
+                  {beatIndex >= 1 && <Text fontSize="xl">Follow the beat!</Text>}
                 </>
               )}
               {/* <IconButton
@@ -370,42 +413,23 @@ export default function CardsForBeats() {
           <ModalHeader>Settings</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text>
-              Label changes are saved to cookies and require you to re-generate
-              the phrase
-            </Text>
+            <Text>Label changes are saved to cookies and require you to re-generate the phrase</Text>
             <br />
             <FormControl>
               <FormLabel>High Hat Label</FormLabel>
-              <Input
-                maxLength={1}
-                defaultValue={userSettingObj.highHatLabel}
-                onChange={highHatLabelChange}
-              />
+              <Input maxLength={1} defaultValue={userSettingObj.highHatLabel} onChange={highHatLabelChange} />
             </FormControl>
             <FormControl>
               <FormLabel>Snare Label</FormLabel>
-              <Input
-                maxLength={1}
-                defaultValue={userSettingObj.snareLabel}
-                onChange={snareLabelChange}
-              />
+              <Input maxLength={1} defaultValue={userSettingObj.snareLabel} onChange={snareLabelChange} />
             </FormControl>
             <FormControl>
               <FormLabel>Bass Label</FormLabel>
-              <Input
-                maxLength={1}
-                defaultValue={userSettingObj.bassLabel}
-                onChange={bassLabelChange}
-              />
+              <Input maxLength={1} defaultValue={userSettingObj.bassLabel} onChange={bassLabelChange} />
             </FormControl>
             <FormControl>
               <FormLabel>Pedal Label</FormLabel>
-              <Input
-                maxLength={1}
-                defaultValue={userSettingObj.pedalLabel}
-                onChange={pedalLabelChange}
-              />
+              <Input maxLength={1} defaultValue={userSettingObj.pedalLabel} onChange={pedalLabelChange} />
             </FormControl>
           </ModalBody>
           <ModalFooter>
